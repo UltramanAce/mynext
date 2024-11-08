e0a85cb0d1118846c5f56dc4b5bc8c7c7b5a7f78
蔡文 <wen.cai@microcloudsys.com>
2024-11-08 13:59:20 +0800

    fix: 修复catch(e)的e的类型定义

--- src/app/api/man/route.ts
+++ src/app/api/man/route.ts
@@ -10,7 +10,13 @@ export const GET = () => {
       }
     });
 
-  } catch (e) {
-    return NextResponse.json({status: 500, message: e.message});
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      // 如果 e 是一个 Error 对象，访问其 message 属性
+      return NextResponse.json({status: 500, message: e.message});
+    } else {
+      // 如果 e 不是 Error 类型
+      console.error("An unknown error occurred");
+    }
   }
 }
