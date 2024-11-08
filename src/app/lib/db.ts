336d623e812a4855842f89ee9e6869aab2eed7cf
蔡文 <wen.cai@microcloudsys.com>
2024-11-08 14:04:16 +0800

    fix: 修复mysql的创建

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,15 +1,14 @@
 import mysql from 'mysql2';
 
-// 创建数据库连接池
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,        // 数据库主机地址
-  user: process.env.DB_USER,        // 数据库用户名
-  password: process.env.DB_PASSWORD, // 数据库密码
-  database: process.env.DB_NAME,    // 使用的数据库名
-  port: process.env.DB_PORT || 3306, // 默认端口3306
-  waitForConnections: true,         // 等待连接
-  connectionLimit: 10,              // 最大连接数
-  queueLimit: 0                     // 连接队列限制
+  host: process.env.DB_HOST,             // 数据库主机地址
+  user: process.env.DB_USER,             // 数据库用户名
+  password: process.env.DB_PASSWORD,     // 数据库密码
+  database: process.env.DB_NAME,         // 数据库名
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306, // 确保 port 是 number 类型
+  waitForConnections: true,              // 是否等待连接池中的连接
+  connectionLimit: 10,                   // 最大连接数
+  queueLimit: 0                          // 最大排队数
 });
 
 // 使用连接池进行查询
