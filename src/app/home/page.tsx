acae1f00e3da433fc38d53795c5a4aa440c5970f
蔡文 <wen.cai@microcloudsys.com>
2024-11-13 11:23:09 +0800

    feat: 试用antd组件

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { Button } from 'antd';
 
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <Button type="primary">Primary</Button>
       <Link href="/user/123">
         跳转到目标页面: user123
       </Link>
