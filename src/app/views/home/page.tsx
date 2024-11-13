37d952bcbf5642760c6deff8bb8306b6cbd88010
蔡文 <wen.cai@microcloudsys.com>
2024-11-13 17:55:56 +0800

    feat: 新增侧边菜单组件

--- src/app/views/home/page.tsx
+++ src/app/views/home/page.tsx
@@ -2,11 +2,15 @@ import '../../styles/common.module.css'
 
 import Search from '../../components/search'
 import PageFooter from '../../components/PageFooter/page'
+import SideMenu from './SideMenu'
 
 export default function Home() {
   return (
     <>
       <Search></Search>
+      <div>
+        <SideMenu></SideMenu>
+      </div>
       <PageFooter></PageFooter>
     </>
   );
