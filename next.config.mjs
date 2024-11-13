f150f93ad04817c00183f9db8e7965c4c22e0543
蔡文 <wen.cai@microcloudsys.com>
2024-11-13 17:09:33 +0800

    feat: 路由重定向，指定url路径

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/home', // 希望显示的url
+        destination: '/views/home', // 实际的文件路径
+        permanent: true // 永久重定向
+      },
+      {
+        source: '/user/:id', // 匹配动态路由 /user/xxx
+        destination: '/views/user/:id', // 重定向到 /views/user/xxx
+        permanent: true // 是否为永久重定向 (301)
+      },
+      {
+        source: '/mall',
+        destination: '/views/mall',
+        permanent: true
+      }
+    ]
+  }
+};
 
 export default nextConfig;
