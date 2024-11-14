d566e4e2029665bbfd4a9ef37af9bc88cc2d1113
蔡文 <wen.cai@microcloudsys.com>
2024-11-14 13:44:02 +0800

    feat: 基本首页已完成

--- src/app/views/home/page.tsx
+++ src/app/views/home/page.tsx
@@ -3,13 +3,15 @@ import '../../styles/common.module.css'
 import Search from '../../components/search'
 import PageFooter from '../../components/PageFooter/page'
 import SideMenu from './SideMenu'
+import Panel1 from '../../components/Panel1/page'
 
 export default function Home() {
   return (
     <>
       <Search></Search>
-      <div>
+      <div className='flex'>
         <SideMenu></SideMenu>
+        <Panel1></Panel1>
       </div>
       <PageFooter></PageFooter>
     </>
