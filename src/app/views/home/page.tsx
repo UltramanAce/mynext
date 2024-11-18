77fe72f65895ed500eacd73de6f614f5624d0082
蔡文 <wen.cai@microcloudsys.com>
2024-11-18 18:13:54 +0800

    feat: 调用商品接口

--- src/app/views/home/page.tsx
+++ src/app/views/home/page.tsx
@@ -5,15 +5,24 @@ import PageFooter from '../../components/pageFooter/page'
 import SideMenu from './sideMenu'
 import Panel1 from '../../components/panel1/page'
 
-export default function Home() {
+const Home = async () => {
+  // 获取商品列表数据
+  const res= await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/product`)
+  const list = await res.json()
+
   return (
     <>
       <Search></Search>
       <div className='flex'>
         <SideMenu></SideMenu>
-        <Panel1></Panel1>
+        { 
+          list.map(() => {
+            return <Panel1></Panel1>
+          }) 
+        }
       </div>
       <PageFooter></PageFooter>
     </>
   );
 }
+export default Home
