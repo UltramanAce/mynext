fb31e0f5d117e3e3abee5472e7cccc52f6be5068
蔡文 <wen.cai@microcloudsys.com>
2024-11-18 17:46:03 +0800

    feat: 新增获取菜单接口并使用

--- src/app/views/home/sideMenu.tsx
+++ src/app/views/home/sideMenu.tsx
@@ -1,84 +1,55 @@
 'use client'
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-const items: MenuItem[] = [
-  {
-    key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
-    children: [
-      {
-        key: 'g1',
-        label: 'Item 1',
-        type: 'group',
-        children: [
-          { key: '1', label: 'Option 1' },
-          { key: '2', label: 'Option 2' },
-        ],
-      },
-      {
-        key: 'g2',
-        label: 'Item 2',
-        type: 'group',
-        children: [
-          { key: '3', label: 'Option 3' },
-          { key: '4', label: 'Option 4' },
-        ],
-      },
-    ],
-  },
-  {
-    key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
-    children: [
-      { key: '5', label: 'Option 5' },
-      { key: '6', label: 'Option 6' },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          { key: '7', label: 'Option 7' },
-          { key: '8', label: 'Option 8' },
-        ],
-      },
-    ],
-  },
-  {
-    type: 'divider',
-  },
-  {
-    key: 'sub4',
-    label: 'Navigation Three',
-    icon: <SettingOutlined />,
-    children: [
-      { key: '9', label: 'Option 9' },
-      { key: '10', label: 'Option 10' },
-      { key: '11', label: 'Option 11' },
-      { key: '12', label: 'Option 12' },
-    ],
-  },
-  {
-    key: 'grp',
-    label: 'Group',
-    type: 'group',
-    children: [
-      { key: '13', label: 'Option 13' },
-      { key: '14', label: 'Option 14' },
-    ],
-  },
-];
-
 const SideMenu: React.FC = () => {
+  const [menus, setMenus] = useState<MenuItem[]>([]);
+
   const onClick: MenuProps['onClick'] = (e) => {
     console.log('click ', e);
   };
+
+  // 转换菜单数据
+  const convertMenus = (data: any) => {
+    const result: MenuItem[] = [];    
+    data.forEach((item: any) => {
+      if (item.children) {
+        result.push({
+          key: item.id,
+          label: item.c_name,
+          icon: item.icon,
+          children: convertMenus(item.children)
+        });
+      } else {
+        result.push({
+          key: item.id,
+          label: item.c_name,
+          icon: item.icon
+        });
+      }
+    });
+    setMenus(result)
+  }
+  // 获取菜单数据
+  const fetchMenus = async () => {
+    try {
+      const res = await fetch('/api/menus');
+      const data = await res.json();
+      convertMenus(data)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchMenus()
+  }, []);
 
   return (
     <Menu
@@ -87,7 +58,7 @@ const SideMenu: React.FC = () => {
       defaultSelectedKeys={['1']}
       defaultOpenKeys={['sub1']}
       mode="inline"
-      items={items}
+      items={menus}
     />
   );
 };
