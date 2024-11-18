a9657dc79fc02b65b9ba901302e7a04216e4f16a
蔡文 <wen.cai@microcloudsys.com>
2024-11-18 18:13:23 +0800

    feat: 新建商品接口

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,22 @@
+import { promisePool } from '../../lib/db';
+
+
+// 获取商品列表
+export async function GET(req) {
+  try {
+    // 这里可以接入数据库获取数据
+    const [data] = await promisePool.query('SELECT * FROM products')
+
+    // 返回菜单列表的响应
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    // 错误处理
+    return new Response(JSON.stringify({ error: '无法获取商品数据' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
